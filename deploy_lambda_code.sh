fd5f408284b81d590b0ff9ea1eb09fa4ac474ada
Elliott Spira <elliott@gorillastack.com>
2015-09-10 13:49:48 +1000

    fixing deploy script.  adding logic to iterate over all records for a cloudtrail event

--- deploy_lambda_code.sh
+++ deploy_lambda_code.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo Zipping code for deployment
-zip code.zip lib/autotag.js
-
-echo Uploading code via AWS CLI
-aws lambda --region ap-northeast-1 update-function-code --function-name arn:aws:lambda:ap-northeast-1:002790823159:function:gs_autotag --zip-file fileb:///home/e/dev/gs/autotag/code.zip
+echo 1. Zipping code for deployment
+pushd lib
+zip -r autotag.zip *
+popd
+mv lib/autotag.zip .
+echo ---
+echo 2. Uploading code via AWS CLI
+aws lambda --region ap-northeast-1 update-function-code --function-name arn:aws:lambda:ap-northeast-1:002790823159:function:gs_autotag --zip-file fileb:///home/e/dev/gs/autotag/autotag.zip
+# aws s3 cp autotag.zip s3://gs-lambda-functions/autotag.zip
