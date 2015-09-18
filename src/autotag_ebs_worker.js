fbc4e9aac61abe4b4d10f6a3ad5ad9fae6c127a0
Elliott Spira <elliott@gorillastack.com>
2015-09-18 11:49:20 +1000

    BIG-216: refactoring workers to extend ec2 worker to maximise code reuse

--- src/autotag_ebs_worker.js
+++ src/autotag_ebs_worker.js
@@ -1,12 +1,7 @@
-const AutotagDefaultWorker = require('./autotag_default_worker');
+const AutotagEC2Worker = require('./autotag_ec2_worker');
 const AWS = require('aws-sdk');
 
-class AutotagEBSWorker extends AutotagDefaultWorker {
-  constructor(event) {
-    super(event);
-    this.ec2 = new AWS.EC2({region: event.awsRegion});
-
-  }
+class AutotagEBSWorker extends AutotagEC2Worker {
   /* tagResource
   ** method: tagResource
   **
@@ -14,26 +9,7 @@ class AutotagEBSWorker extends AutotagDefaultWorker {
   */
 
   tagResource() {
-    let _this = this;
-    return new Promise(function(resolve, reject) {
-      try {
-        _this.ec2.createTags({
-          Resources: [
-            _this.getVolumeId()
-          ],
-          Tags: [
-            _this.getAutotagPair()
-          ]
-        }, function(err, res) {
-          if (err)
-            reject(err);
-          else
-            resolve(true);
-        });
-      } catch(e) {
-        reject(e);
-      }
-    });
+    return this.tagEC2Resources([this.getVolumeId()]);
   }
 
   getVolumeId() {
--- src/autotag_internet_gateway_worker.js
+++ src/autotag_internet_gateway_worker.js
@@ -1,12 +1,7 @@
-const AutotagDefaultWorker = require('./autotag_default_worker');
+const AutotagEC2Worker = require('./autotag_ec2_worker');
 const AWS = require('aws-sdk');
 
-class AutotagInternetGatewayWorker extends AutotagDefaultWorker {
-  constructor(event) {
-    super(event);
-    this.ec2 = new AWS.EC2({region: event.awsRegion});
-
-  }
+class AutotagInternetGatewayWorker extends AutotagEC2Worker {
   /* tagResource
   ** method: tagResource
   **
@@ -14,27 +9,7 @@ class AutotagInternetGatewayWorker extends AutotagDefaultWorker {
   */
 
   tagResource() {
-    let _this = this;
-    return new Promise(function(resolve, reject) {
-      console.log('trying to tag internet gateway');
-      try {
-        _this.ec2.createTags({
-          Resources: [
-            _this.getInternetGatewayId()
-          ],
-          Tags: [
-            _this.getAutotagPair()
-          ]
-        }, function(err, res) {
-          if (err)
-            reject(err);
-          else
-            resolve(true);
-        });
-      } catch(e) {
-        reject(e);
-      }
-    });
+    return this.tagEC2Resources([this.getInternetGatewayId()]);
   }
 
   getInternetGatewayId() {
